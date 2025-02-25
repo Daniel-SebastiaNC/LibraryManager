6dff54b2d2e549247824c678eb2514a3b3047c60
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-02-25 16:50:58 -0300

    feat: adiciona BookMapper para converter a entidade Book e seus DTO's

---
+++ src/main/java/br/com/dev/danielsebastian/librarymanager/mapper/BookMapper.java
@@ -0,0 +1,28 @@
+package br.com.dev.danielsebastian.librarymanager.mapper;
+
+import br.com.dev.danielsebastian.librarymanager.controller.request.BookRequest;
+import br.com.dev.danielsebastian.librarymanager.controller.response.BookResponse;
+import br.com.dev.danielsebastian.librarymanager.entity.Book;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class BookMapper {
+    public static Book toBook(BookRequest bookRequest){
+        return Book.builder()
+                .title(bookRequest.title())
+                .author(bookRequest.author())
+                .description(bookRequest.description())
+                .status(true)
+                .build();
+    }
+
+    public static BookResponse toBookResponse(Book book){
+        return BookResponse.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .author(book.getAuthor())
+                .description(book.getDescription())
+                .status(book.isStatus())
+                .build();
+    }
+}
