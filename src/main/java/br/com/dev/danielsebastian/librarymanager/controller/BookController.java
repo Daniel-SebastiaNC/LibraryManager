873504da60fc891ff94b85d404cf2fb3cd7d27f0
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-02-25 18:05:58 -0300

    feat: adicona o metodo POST para addBook na camada Controller e Service da entidade Book

--- src/main/java/br/com/dev/danielsebastian/librarymanager/controller/BookController.java
+++ src/main/java/br/com/dev/danielsebastian/librarymanager/controller/BookController.java
@@ -1,13 +1,26 @@
 package br.com.dev.danielsebastian.librarymanager.controller;
 
+import br.com.dev.danielsebastian.librarymanager.controller.request.BookRequest;
+import br.com.dev.danielsebastian.librarymanager.controller.response.BookResponse;
 import br.com.dev.danielsebastian.librarymanager.service.BookService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/book")
 @RequiredArgsConstructor
 public class BookController {
 
     private final BookService bookService;
+
+    @PostMapping("/add")
+    public ResponseEntity<BookResponse> addBook(@RequestBody BookRequest bookRequest){
+        return ResponseEntity.status(HttpStatus.CREATED).body(bookService.addBook(bookRequest));
+    }
 
 }
--- src/main/java/br/com/dev/danielsebastian/librarymanager/service/BookService.java
+++ src/main/java/br/com/dev/danielsebastian/librarymanager/service/BookService.java
@@ -1,5 +1,9 @@
 package br.com.dev.danielsebastian.librarymanager.service;
 
+import br.com.dev.danielsebastian.librarymanager.controller.request.BookRequest;
+import br.com.dev.danielsebastian.librarymanager.controller.response.BookResponse;
+import br.com.dev.danielsebastian.librarymanager.entity.Book;
+import br.com.dev.danielsebastian.librarymanager.mapper.BookMapper;
 import br.com.dev.danielsebastian.librarymanager.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,7 +11,12 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class BookService {
+
     private final BookRepository bookRepository;
 
-
+    public BookResponse addBook(BookRequest bookRequest){
+        Book book = BookMapper.toBook(bookRequest);
+        Book saveBook = bookRepository.save(book);
+        return BookMapper.toBookResponse(saveBook);
+    }
 }
