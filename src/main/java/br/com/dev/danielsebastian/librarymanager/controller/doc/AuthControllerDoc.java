5075efeaa812c4a56297dc4ef1fa5661b1c47573
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-03-05 21:12:48 -0300

    fix: retira o ApiResponse do AuthControllerDoc para deixar o padrao

--- src/main/java/br/com/dev/danielsebastian/librarymanager/controller/doc/AuthControllerDoc.java
+++ src/main/java/br/com/dev/danielsebastian/librarymanager/controller/doc/AuthControllerDoc.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
@@ -17,22 +16,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 @Tag(name = "Auth", description = "Recurso responsável pela autenticação e registro de Administrador.")
 public interface AuthControllerDoc {
     @Operation(summary = "Registrar Administrador",
-            description = "Método responsável por cadastrar o administrador no sistema.",
-            security = @SecurityRequirement(name = "bearerAuth"))
+            description = "Método responsável por cadastrar o administrador no sistema.")
     @ApiResponse(responseCode = "201",
             description = "Retorna o administrador cadastrado.",
             content = @Content(schema = @Schema(implementation = UserResponse.class)))
-    @ApiResponse(responseCode = "400",
-            content = @Content(schema = @Schema(implementation = String.class)))
     ResponseEntity<UserResponse> registerUser(@Valid @RequestBody UserRequest userRequest);
 
     @Operation(summary = "Autenticação de Administrador",
-            description = "Método responsável por autenticar administrador.",
-            security = @SecurityRequirement(name = "bearerAuth"))
+            description = "Método responsável por autenticar administrador.")
     @ApiResponse(responseCode = "200",
             description = "Retorna token de acesso.",
             content = @Content(schema = @Schema(implementation = UserLoginResponse.class)))
-    @ApiResponse(responseCode = "400",
-            content = @Content(schema = @Schema(implementation = String.class)))
     ResponseEntity<UserLoginResponse> login(@Valid @RequestBody UserLoginRequest userLoginRequest);
 }
